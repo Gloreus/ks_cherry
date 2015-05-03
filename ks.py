67f264d4d31cbdc0b59938c9504069e2c482479c
gloreus <gloreus@gmail.com>
2015-05-03 15:52:03 +0300

    Разнесены шаблоны, обработка страниц

--- ks.py
+++ ks.py
@@ -4,8 +4,8 @@ import cherrypy
 class ks_site:
     @cherrypy.tools.template(name='index')
     def index(self):
-        # CherryPy will call this method for the root URI ("/") and send
-        # its return value to the client. Because this is tutorial
-        # lesson number 01, we'll just send something really simple.
-        # How about...
         return {'site_title': '12'}
+
+    @cherrypy.tools.template(name='film_review')
+    def film_review(self, id=''):
+        return {'site_title': id}
---
+++ start.bat
@@ -0,0 +1,3 @@
+d:
+cd d:\WORK\ks_cherry\
+d:\WORK\ks_cherry\PY\Scripts\python.exe app.py > app.log
